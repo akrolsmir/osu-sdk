e8d4050508d6d7718688d14da6ae6fe7c5c52852
Austin Chen <akrolsmir@gmail.com>
2012-04-08 12:13:42 -0700

    More logic and comments for AiSnapping.
    (Still needs access to red points/timing to determine if snapped).

--- osu!framework/GameModes/Edit/AiMod/rulesets/AiSnapping.cs
+++ osu!framework/GameModes/Edit/AiMod/rulesets/AiSnapping.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using osu.GameplayElements.HitObjects;
-using osu.GameModes.Edit.AiMod.Reports;
-using osu.GameModes.Edit.AiMod.Rulesets;
 
-//how to AutoComment
-//how to Organize imports
+//how to AutoComment - ///
+//how to Organize imports - Edit > Intilliesense > Organize Usings
 //What is getType?
-//how to Autoformat
+//how to Autoformat - ctrl e + d
 //What are [Flags]
 //What is Delegate?
 //What replaces super?
@@ -16,15 +12,18 @@ using osu.GameModes.Edit.AiMod.Rulesets;
 
 namespace osu.GameModes.Edit.AiMod.Rulesets
 {
-    internal class AiSnapping:AiModRuleset
+    internal class AiSnapping : AiModRuleset
     {
         //Type = AiModType.Snapping;
+        //TODO figure out how to set type to Snapping.
+        //public AiSnapping()
+        //{
+        //}
 
-        public AiSnapping()
-        {
-            
-        }
-
+        /// <summary>
+        /// Goes through the list of hitObjects sorting them based on type.
+        /// </summary>
+        /// <param name="hitObjects"></param>
         protected override void RunAllRules(List<HitObjectBase> hitObjects)
         {
             for (int i = 0; i < hitObjects.Count; i++)
@@ -50,33 +49,91 @@ namespace osu.GameModes.Edit.AiMod.Rulesets
             //throw new NotImplementedException();
         }
 
-        private void testSliderSnap(HitObjectBase currentHitObject)
-        {
-            //Check start, in between, and end time
-        }
-
-
-        private void testSpinnerSnap(HitObjectBase currentHitObject)
-        {
-            //Check start and end time
-        }
-
+        /// <summary>
+        /// For circles. Checks for start time snapping only.
+        /// </summary>
+        /// <param name="currentHitObject"></param>
         private void testNormalSnap(HitObjectBase currentHitObject)
         {
-            //Check start time
+            int startTime = currentHitObject.StartTime;
+            if (!isSnapped(startTime))
+            {
+                Reports.Add(SnappingReport(startTime, "Circle isn't snapped."));
+            }
+
         }
 
-        private bool testFine()
+        /// <summary>
+        /// For spinners. Checks for start and end time snapping.
+        /// </summary>
+        /// <param name="currentHitObject"></param>
+        private void testSpinnerSnap(HitObjectBase currentHitObject)
+        {
+            int startTime = currentHitObject.StartTime;
+            int endTime = currentHitObject.EndTime;
+            if (!isSnapped(startTime))
+            {
+                Reports.Add(SnappingReport(startTime, "Spinner isn't snapped."));
+            }
+            else if (!isSnapped(endTime))
+            {
+                Reports.Add(SnappingReport(startTime, "Spinner's end isn't snapped."));
+            }
+        }
+
+        /// <summary>
+        /// For sliders. Checks for start, repeats, and end time snapping.
+        /// </summary>
+        /// <param name="currentHitObject"></param>
+        private void testSliderSnap(HitObjectBase currentHitObject)
+        {
+            int startTime = currentHitObject.StartTime;
+            int endTime = currentHitObject.EndTime;
+            if (!isSnapped(startTime))
+            {
+                Reports.Add(SnappingReport(startTime, "Slider isn't snapped."));
+            }
+            //TODO for each repeat, if repeat isn't snapped...
+            else if (!isSnapped(endTime))
+            {
+                Reports.Add(SnappingReport(startTime, "Slider's end isn't snapped."));
+            }
+
+        }
+
+        private bool test(int time)
         {
             return true;
+            //return if time is snapped
         }
 
-        private AiReportOneObject SnappingReport (HitObjectBase h1, string information)
+        private bool test()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if this time is snapped.
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        private bool isSnapped(int time)
+        {
+            //TODO fill
+        }
+
+        /// <summary>
+        /// A customized report for snapping errors.
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="information"></param>
+        /// <returns></returns>
+        private AiReport SnappingReport(int time, string information)
         {
             //TODO understand delegate
             //TODO get weblink
-            BeenCorrectedDelegate d = new BeenCorrectedDelegate(testFine);
-            return new AiReportOneObject(h1, d, Severity.Error, information, 0);
+            //BeenCorrectedDelegate d = new BeenCorrectedDelegate(testFine);
+            return new AiReport(time, Severity.Error, information, 0, new BeenCorrectedDelegate(test(time)));
         }
     }
 
