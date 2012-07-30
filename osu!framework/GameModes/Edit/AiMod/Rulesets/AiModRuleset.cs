799148cb1c6930849406e7dc48d7818fac07a52d
Austin Chen <akrolsmir@gmail.com>
2012-07-30 14:41:03 -0700

    merged?

---
+++ osu!framework/GameModes/Edit/AiMod/Rulesets/AiModRuleset.cs
@@ -0,0 +1,108 @@
+<<<<<<< HEAD:osu!framework/GameModes/Edit/AiMod/AiModRuleset.cs
+﻿using System;
+using System.Collections.Generic;
+using osu.GameplayElements.HitObjects;
+
+namespace osu.GameModes.Edit.AiMod
+{
+    public abstract class AiModRuleset : MarshalByRefObject
+    {
+        public List<AiReport> Reports;
+
+        /// <summary>
+        /// Set a default type for reports returned by this ruleset.
+        /// </summary>
+        public abstract AiModType Type { get; }
+
+        public AiModRuleset()
+        {
+            Reports = new List<AiReport>();
+        }
+
+        public List<AiReport> Run(List<HitObjectBase> hitObjects)
+        {
+            Reports.Clear();
+            RunAllRules(hitObjects);
+            return Reports;
+        }
+
+        /// <summary>
+        /// Runs all rules for this ruleset and fills Reports.
+        /// </summary>
+        protected abstract void RunAllRules(List<HitObjectBase> hitObjects);
+    }
+
+    public enum BeatmapDifficulty
+    {
+        Easy,
+        Normal,
+        Hard,
+        Insane,
+        Expert
+    }
+
+    public enum AiModType
+    {
+        All = 0,
+        Spacing,
+        Snapping,
+        Errors,
+        Difficulty,
+        Style,
+        Custom
+    }
+=======
+﻿using System;
+using System.Collections.Generic;
+using osu.GameplayElements.HitObjects;
+
+namespace osu.GameModes.Edit.AiMod.Rulesets
+{
+    public abstract class AiModRuleset
+    {
+        public List<AiReport> Reports;
+
+        /// <summary>
+        /// Set a default type for reports returned by this ruleset.
+        /// </summary>
+        public abstract AiModType Type { get; }
+
+        public AiModRuleset()
+        {
+            Reports = new List<AiReport>();
+        }
+
+        public List<AiReport> Run(List<HitObjectBase> hitObjects)
+        {
+            Reports.Clear();
+            RunAllRules(hitObjects);
+            return Reports;
+        }
+
+        /// <summary>
+        /// Runs all rules for this ruleset and fills Reports.
+        /// </summary>
+        protected abstract void RunAllRules(List<HitObjectBase> hitObjects);
+    }
+
+    public enum BeatmapDifficulty
+    {
+        Easy,
+        Normal,
+        Hard,
+        Insane,
+        Expert
+    }
+
+    public enum AiModType
+    {
+        All = 0,
+        Spacing,
+        Snapping,
+        Errors,
+        Difficulty,
+        Style,
+        Custom
+    }
+>>>>>>> test3:osu!framework/GameModes/Edit/AiMod/Rulesets/AiModRuleset.cs
+}
---
+++ osu!framework/GameModes/Edit/AiMod/Rulesets/SnappingRuleset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu.GameModes.Edit.AiMod.Rulesets
+{
+    class SnappingRuleset
+    {
+
+
+    }
+}
